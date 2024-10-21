cd8a513bc420ff5cadb85d469575844c5df3bccd
sfaa <ym6772003@gmail.com>
2024-10-21 14:20:49 -0700

    border

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -66,50 +66,42 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {" "}
+                {project.num}{" "}
+              </div>
+              {/* project category */}
+
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category}project{" "}
+              </h2>
+              {/* project description */}
+              <p className="text-white/60">{project.description}</p>
+              {/* stock  */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {/* remove the Lost commo */}
+                      {index !== project.stack.length - 1 && ","}{" "}
+                    </li>
+                  );
+                })}{" "}
+              </ul>
+
+              {/* border */}
+<div className="border border-white/20"></div>
 
 
-
-
-
-
-     <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-      <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none"> 
-        <div> 
-          {/* outline num */}
-          <div className="text-8xl leading-none font-extrabold text-transparent text-outline"> {project.num} </div> 
-          {/* project category */}
-        
-
-<h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-   {project.category }project </h2>
-    {/* project description */} 
-    <p className="text-white/60">{project.description}</p>
-     {/* stock  */}
-     <ul className="flex gap-4">
-        {project.stack.map((item, index) => 
-      { return ( 
-
-
-<li key={index} className="text-xl text-accent">
-   {item.name} 
-   {/* remove the Lost commo */}
-    {index !== project.stack.length -1 && ","} </li>
-
-
-
-      ); })}  </ul>
-
-
-
-
+            </div>
           </div>
-           </div> 
           <div className="w-full xl:w-[50%]">slider</div>
-          
-          </div>
-
-
-
+        </div>
       </div>
     </motion.section>
   );

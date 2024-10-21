fcfd587ad4e3bdaa40ffa25ed3e1c33bd879e4c5
sfaa <ym6772003@gmail.com>
2024-10-21 11:25:12 -0700

    finsh-exp add-skils

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -183,10 +183,11 @@ const Resume = () => {
                return <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
                 <span className="text-accent">{item.duration}</span>
                 <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left ">  {item.institution}</h3>
-                <div>
+                <div className=" flex items-center gap-3">
                   {/* dot */}
-                  <span ></span>
-                  <p>{item.degree}</p>
+                  <span className="w-[6px] h-[6px] rounded-full
+                   bg-accent"></span>
+                  <p className="text-white/60">{item.degree}</p>
                 </div>
                </li>
 
@@ -207,7 +208,16 @@ const Resume = () => {
 
             {/* skils */}
             <TabsContent value="skills" className="w-full">
-              skills
+            <div className="flex flex-col gap-[30px]">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{skills.title}</h3>
+<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{skills.description}</p>
+                
+                <ul>{skills.skillsList.map((skills,index)=> {
+                  return <li key={index}>{skills.name}</li>
+                })}</ul>
+              </div>
+            </div>
             </TabsContent>
 
             {/* about */}

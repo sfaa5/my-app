659902debfb62ce382f4a294ad7dec6a2237a711
sfaa <ym6772003@gmail.com>
2024-10-21 14:02:16 -0700

    motion work

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -29,31 +29,45 @@ const projects = [
     github: "",
   },
   {
-    num: "0",
-    category: "frontend",
-    title: "project 1",
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
-    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
-    image: "/assets/work/thumb1.png",
-    live: "",
-    github: "",
-  },  {
-    num: "01",
-    category: "frontend",
-    title: "project 1",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
-    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "node.js" }],
     image: "/assets/work/thumb1.png",
     live: "",
     github: "",
   },
-  
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [
+      { name: "Next.js" },
+      { name: "Tailwind.css" },
+      { name: "Javascript" },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
 ];
 
 const Work = () => {
-  return <div>work page</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">projects</div>
+    </motion.section>
+  );
 };
 
 export default Work;

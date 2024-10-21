a10b51583ea00e562600e6966dbe5886f12eb772
sfaa <ym6772003@gmail.com>
2024-10-21 11:57:30 -0700

    style li

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -248,9 +248,9 @@ const Resume = () => {
                 <h3 className="text-4xl font-bold">{about.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx:-0">{about.description}</p>
                 <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
-                  {about.info.map((item,index)=>{
+                  {about.info.map((item,index)=>{  
                     return(
-                      <li key={index}>
+                      <li key={index} className="flex items-center justify-center xl:justify-start gap-4 ">
 <span>{item.filedName}</span>
 <span>{item.filedValue}</span>
                       </li>

6419e15ae46afc546e5bb9087a961faa9c26a5e0
sfaa <ym6772003@gmail.com>
2024-10-21 14:08:59 -0700

    outline num

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -14,7 +14,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/ components/ui/tooltip";
+} from "@/components/ui/tooltip";
 
 const projects = [
   {
@@ -65,7 +65,27 @@ const Work = () => {
       animate={{ opacity: 1 }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
-      <div className="container mx-auto">projects</div>
+      <div className="container mx-auto">
+
+
+
+
+
+
+     <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+      <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none"> 
+        <div> 
+          {/* outline num */}
+          <div className="text-8xl leading-none font-extrabold text-transparent text-outline"> {project.num} </div> 
+          </div>
+           </div> 
+          <div className="w-full xl:w-[50%]">slider</div>
+          
+          </div>
+
+
+
+      </div>
     </motion.section>
   );
 };

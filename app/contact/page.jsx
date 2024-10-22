ad9533c309252eb1a55cc34dd012d99dab3ee459
sfaa <ym6772003@gmail.com>
2024-10-22 07:54:11 -0700

    motion

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -12,25 +12,44 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-
-
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
- const info = [ 
-   {  icon: <FaPhoneAlt />,
-     title: "Phone",
-      description: "(+40) 321 654 876",
-      },   {  icon: <FaEnvelope />,
-        title: "Email",
-         description: "ym6772003@gmail.com",
-         },  {  icon: <FaMapMarkerAlt />,
-          title: "Addrss",
-           description: "Downtown, Cairo, Egypt",
-           }, ];
+const info = [
+  { icon: <FaPhoneAlt />, title: "Phone", description: "(+40) 321 654 876" },
+  { icon: <FaEnvelope />, title: "Email", description: "ym6772003@gmail.com" },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Addrss",
+    description: "Downtown, Cairo, Egypt",
+  },
+];
 
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return <motion.div> contact page</motion.div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form>form</form>
+          </div>
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+ 
+    </motion.section>
+  );
 };
 
 export default Contact;

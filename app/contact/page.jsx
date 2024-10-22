54210c45d0dbd1c30ea65e3110d701a6b896bf2e
sfaa <ym6772003@gmail.com>
2024-10-22 07:46:54 -0700

    work info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,36 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
+
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+ const info = [ 
+   {  icon: <FaPhoneAlt />,
+     title: "Phone",
+      description: "(+40) 321 654 876",
+      },   {  icon: <FaEnvelope />,
+        title: "Email",
+         description: "ym6772003@gmail.com",
+         },  {  icon: <FaMapMarkerAlt />,
+          title: "Addrss",
+           description: "Downtown, Cairo, Egypt",
+           }, ];
+
+import {motion} from "framer-motion"
 
 const Contact = () => {
-  return (
-    <div> contact page</div>
-  )
-}
+  return <motion.div> contact page</motion.div>;
+};
 
-export default Contact
+export default Contact;
